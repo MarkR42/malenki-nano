1148456c513699ad896b4b14347cf04d6e4e3646
Mark <markxr@gmail.com>
2019-03-06 18:37:16 +0000

    More firmware experiments

---
+++ firmware/demo1/diag.h
@@ -0,0 +1,5 @@
+// Write a string
+void diag_puts(const char *str);
+
+void diag_printf(const char * fmt, ...);
+
--- firmware/demo1/main.c
+++ firmware/demo1/main.c
@@ -5,11 +5,46 @@
 #include <util/delay.h>
 
 #include <string.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+#include "diag.h"
 
 char unused1;
 unsigned char somestorage[10];
 
-void init_hardware()
+
+void init_hardware_rx()
+{
+	// UART0- need to use "alternate" pins 
+	// This puts TxD and RxD on PA1 and PA2
+	PORTMUX.CTRLB = PORTMUX_USART0_ALTERNATE_gc; 
+
+	// Timer B TCB0 is used to measure the pulse width.
+	// RxD pin (PA2) 
+	PORTA.DIRCLR = 1 << 2;
+	// EVENTSYS - use ASYNC channel 0 (works with porta)
+	EVSYS.ASYNCCH0 = EVSYS_ASYNCCH0_PORTA_PIN2_gc;
+	// routing for asynch user 0 == TCB0
+	EVSYS.ASYNCUSER0 = EVSYS_ASYNCUSER0_ASYNCCH0_gc;
+	// TODO:Configure TCB0
+
+	// Diagnostic uart output	
+	// TxD pin PA1 is used for diag, should be an output
+	PORTA.DIRSET = 1 << 1;
+	// Set baud rate etc
+	USART0.CTRLC =
+        	USART_CMODE_ASYNCHRONOUS_gc | // Mode: Asynchronous[default]
+        	USART_PMODE_DISABLED_gc | // Parity: None[default]
+        	USART_SBMODE_1BIT_gc | // StopBit: 1bit[default]
+		USART_CHSIZE_8BIT_gc; // CharacterSize: 8bit[default]
+	uint32_t want_baud_hz = 9600; // Baud rate
+	uint32_t clk_per_hz = 3333L * 1000; // CLK_PER after prescaler in hz
+	uint16_t baud_param = (64 * clk_per_hz) / (16 * want_baud_hz);
+	USART0.BAUD = baud_param;
+}
+
+void init_hardware_motors()
 {
 	// Set all the ports which need to be outputs, to be outputs.
 	// should all be low initially.
@@ -23,37 +58,33 @@ void init_hardware()
 	PORTB.DIRSET = 1 << 0;
 	PORTB.DIRSET = 1 << 1;
 	PORTB.DIRSET = 1 << 2;
-
-	// Timer A = generate PWM on thos pins
-	
-	// UART0- need to use "alternate" pins 
-	// This puts TxD and RxD on PA1 and PA2
-	PORTMUX.CTRLB = 1; // Bit 0 = alt. usart pins
-
-	// TxD pin is used for debug, should be an output
-	PORTA.DIRSET = 1 << 1;
-
 	//
 	// Timer A should be in split mode
 	//
 }
 
+void init_hardware()
+{
+	init_hardware_motors();
+	init_hardware_rx();
+}
+
 void init_timer_interrupts()
 {
-	// This uses TCB0 for timer interrupts.
+	// This uses TCB1 for timer interrupts.
 	const unsigned short compare_value = 33333; // 100hz
-	TCB0.CCMP = compare_value;
-	TCB0.INTCTRL = TCB_CAPT_bm;
+	TCB1.CCMP = compare_value;
+	TCB1.INTCTRL = TCB_CAPT_bm;
 	// CTRLB bits 0-2 are the mode, which by default
 	// 000 is "periodic interrupt" -which is correct
-	TCB0.CTRLA = TCB_ENABLE_bm;
+	TCB1.CTRLA = TCB_ENABLE_bm;
 }
 
 volatile uint16_t tickcount = 0;
-ISR(TCB0_INT_vect)
+ISR(TCB1_INT_vect)
 {
     tickcount++;
-    TCB0.INTFLAGS |= TCB_CAPT_bm; //clear the interrupt flag(to reset TCB0.CNT)
+    TCB1.INTFLAGS |= TCB_CAPT_bm; //clear the interrupt flag(to reset TCB0.CNT)
 }
 
 int main(void)
@@ -62,6 +93,10 @@ int main(void)
 	init_hardware();	
 	init_timer_interrupts();
 	sei();  // enable interrupts
-	_delay_ms(100);
+	diag_puts("Hello\n");
+	for (int i=0; i<20; i++) {
+		_delay_ms(100);
+		diag_printf("got %d timer interrupts\n", tickcount);	
+	}
 	while(1) ;
 }
