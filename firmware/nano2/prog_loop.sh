a1396886bc3305a8aa7907152acf57f4241dfdef
Mark Robson <markxr@gmail.com>
2020-06-14 10:47:29 +0100

    Radio: message for radio fail programming loop thing

--- firmware/nano2/prog_loop.sh
+++ firmware/nano2/prog_loop.sh
@@ -2,8 +2,8 @@
 opts="-d tiny1617 -c /dev/ttyUSB0 -b 115200" 
 set -x
 while true; do
-    pyupdi.py $opts --progmode -f obj/main.hex &&
-        pyupdi.py $opts -fs 128:0x2a &&
+    # Write fuse 128 (0x80) - which is USERROW0
+    pyupdi.py $opts -f obj/main.hex -fs 128:0x2a &&
             sleep 30
     sleep 3s
 done
