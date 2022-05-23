e9a8cc8771129b3a5579cc0c231ff124214e6970
Mark Robson <markxr@gmail.com>
2022-05-23 20:56:05 +0100

    Panel of 12 pieces with the 1616 chip

--- hardware/panel12-1616/makepanel.sh
+++ hardware/panel12-1616/makepanel.sh
@@ -2,8 +2,8 @@ kikit panelize \
     --framing 'frame; width: 4mm; space: 3mm; cuts:h' \
     --layout 'grid; rows: 3; cols: 4; vspace: 10mm; hbackbone: 4mm; hspace:3mm' \
     --tabs 'fixed; width: 3mm' \
-    --cuts 'mousebites; drill: 0.5mm; spacing: 1mm; offset: -0.2mm' \
-    --post 'millradius: 1.5mm' \
+    --cuts 'mousebites; drill: 0.5mm; spacing: 1mm; offset: -0.2mm; prolong: 1.5mm' \
+    --post 'millradius: 1.5mm; copperfill: true' \
     --source 'tolerance: 8mm' \
     ../malenki-1616/malenki-1616.kicad_pcb panel12-1616.kicad_pcb
     
