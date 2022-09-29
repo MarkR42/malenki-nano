016b73c119a84e17ced20153e3644376133548e0
Mark Robson <markxr@gmail.com>
2022-09-29 21:26:55 +0100

    Malenki 1616 panel update

--- hardware/panel12-1616/makepanel.sh
+++ hardware/panel12-1616/makepanel.sh
@@ -1,9 +1,10 @@
 kikit panelize \
     --framing 'frame; width: 4mm; space: 3mm; cuts:h' \
-    --layout 'grid; rows: 3; cols: 4; vspace: 10mm; hbackbone: 4mm; hspace:3mm' \
-    --tabs 'fixed; width: 3mm' \
-    --cuts 'mousebites; drill: 0.5mm; spacing: 1mm; offset: -0.2mm; prolong: 1.5mm' \
+    --layout 'grid; rows: 3; cols: 4; vspace: 3mm; hbackbone: 6mm; hspace:3mm' \
+    --tabs 'annotation' \
+    --cuts 'mousebites; drill: 0.3mm; spacing: 0.6mm; offset: -0.2mm; prolong: 1.5mm' \
     --post 'millradius: 1.5mm; copperfill: true' \
     --source 'tolerance: 8mm' \
+    --text 'simple; width:2mm; height:2mm; voffset: 2mm; text: Malenki Panel12 Sep 2022' \
     ../malenki-1616/malenki-1616.kicad_pcb panel12-1616.kicad_pcb
     
